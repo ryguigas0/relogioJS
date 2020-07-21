5eaca19f6edcc10395dfee4d442e4b0de3c0aefd
Ryguigas <32659090+Ryguigas@users.noreply.github.com>
2020-07-21 19:31:12 -0300

    Add files via upload

---
+++ horario.js
@@ -0,0 +1,21 @@
+var hora = new Date().getHours()
+var imagem = document.getElementById("img")
+
+function load(){
+    var div = document.getElementById("horario")
+    div.innerText = `Agora são ${hora} hora(s).`
+    if(hora >= 5 && hora < 12){
+        div.innerText += " Bom dia!"
+        imagem.src = "imagens/manha.jpg"
+        document.body.style.background = "#F3DB95"
+    }else if(hora >= 12 && hora < 18){
+        div.innerText += " Boa tarde!"
+        imagem.src = "imagens/tarde.jpg"
+        document.body.style.background = "#FC7C03"
+    }else {
+        div.innerText += " Boa noite!"
+        imagem.src = "imagens/noite.jpg"
+        document.body.style.background = "#060621"
+        document.body.style.color = "white"
+    }
+}
